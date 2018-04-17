bb0cc885ef01fce55b63b3e2f13339e488302e10
sammieliang <32041223+sammieliang@users.noreply.github.com>
2018-04-17 12:58:11 -0400

    fixed generate_data function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,7 +1,11 @@
 generate_data = function(n, p)
 {
-  covariates = rnorm(n*p, 0, 1)
-  covariates = matrix(draws, nrow=n, ncol=p)
   responses = rnorm(n, 0, 1)
-  return (list(covariates, responses))
+  covariates = matrix(responses, nrow=n, ncol=p)
+  
+  res = list(covariates, responses)
+  names(res) = c("covariates", "responses")
+  return (res)
 }
+
+
