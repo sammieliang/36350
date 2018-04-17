4d1962288d435b84f14370bb344d8837d37e9605
sammieliang <32041223+sammieliang@users.noreply.github.com>
2018-04-17 13:06:25 -0400

    implemented model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,4 +8,19 @@ generate_data = function(n, p)
   return (res)
 }
 
+model_select = function(covariates, responses, cutoff)
+{
+  linear.reg = lm(responses ~ covariates)
+  p-vals = summary(linear.reg)$coefficients[, 4]
+  linear.reg2 = lm(responses ~ covariates[p-vals <= cutoff])
+  p-vals.retained = summary(linear.reg2)$coefficients[, 4]
+  if (length(p-vals.retained == 0))
+  {
+    return (vector(length=0))
+  }
+  else
+  {
+    return (p-vals.retained)
+  }
+}
 
