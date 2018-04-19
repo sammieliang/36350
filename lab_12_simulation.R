a44b3d9f1cfc29159fdcd84724e63d1f5ad7cb4e
sammieliang <32041223+sammieliang@users.noreply.github.com>
2018-04-19 18:18:31 -0400

    changes

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,7 +1,7 @@
 generate_data = function(n, p)
 {
   responses = rnorm(n, 0, 1)
-  covariates = matrix(responses, nrow=n, ncol=p)
+  covariates = matrix(rnorm((n*p), 0, 1), nrow=n, ncol=p)
   
   res = list(covariates, responses)
   names(res) = c("covariates", "responses")
@@ -11,16 +11,34 @@ generate_data = function(n, p)
 model_select = function(covariates, responses, cutoff)
 {
   linear.reg = lm(responses ~ covariates)
-  p-vals = summary(linear.reg)$coefficients[, 4]
-  linear.reg2 = lm(responses ~ covariates[p-vals <= cutoff])
-  p-vals.retained = summary(linear.reg2)$coefficients[, 4]
-  if (length(p-vals.retained == 0))
+  x = summary(linear.reg)$coefficients[-1, 4]
+  bool.vec = x <= cutoff
+  if (sum(bool.vec) == 0)
   {
     return (vector(length=0))
   }
-  else
+  linear.reg2 = lm(responses ~ covariates[,bool.vec])
+  print("Here!")
+  retained = summary(linear.reg2)$coefficients[-1, 4]
+  return (retained)
+}
+
+run_simulation = function(n_trials, n, p, cutoff)
+{
+  
+  for (curr_n in n)
   {
-    return (p-vals.retained)
+    for (curr_p in p)
+    {
+      data.list = generate_data(curr_n, curr_p)
+      vals = model_select(data.list[["covariates"]], data.list[["responses"]], cutoff)
+      if (length(vals) != 0)
+      {
+        hist(vals)
+      }
+    }
   }
 }
+
+run_simulation(5, c(100, 1000, 10000), p=c(10, 20, 50), cutoff=0.05)
 
